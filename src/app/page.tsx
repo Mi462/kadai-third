f8e4f3761188b291f789303beb27f7966e7d71ba
yumi_23817 <candyclover.12815@gmail.com>
2023-08-17 19:37:14 +0900

    Todoリスト質問2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,35 @@
 'use client'
 
-import { useRouter } from "next/navigation"
+//import { useRouter } from "next/navigation"
 import AddTodo from "./components/AddTodo"
+import Link from "next/link";
 
 
 export default function Home() {
 
-  const router = useRouter();
+  //const router = useRouter();
   
   return (
     <div className="flex flex-col h-screen">
-      <header className="text-2xl font-bold bg-blue-500 text-white text-left p-2">
+      <header className="space-between  text-2xl font-bold bg-blue-500 text-white text-left p-2">
         <h1>Todo List</h1>
     </header>
 
       <div className="mx-auto my-3 w-9/12 bg-whiteborder rounded-lg border-gray-300 border-2 text-center p-2">
 
+          <Link href= "/AddTodo">
+            <button 
+              type="button" 
+              className="py-3 px-4 inline-flex justify-right items-center gap-2 rounded-md border      border-transparent font-semibold text-blue-500 hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-white-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
+            >
+              Add Todo
+            </button>
+          </Link>
+
           <button 
             type="button" 
-            className="py-3 px-4 inline-flex justify-right items-center gap-2 rounded-md border      border-transparent font-semibold text-blue-500 hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-white-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
-            >
-            Add Todo
+            className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold text-yellow-500 hover:bg-yellow-100 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">
+            Logout
           </button>
         
         <ul className='space-y-3'>
@@ -84,12 +93,10 @@ export default function Home() {
       
 
       <div className="mx-auto my-3 w-9/12 bg-whiteborder rounded-lg border-gray-300 border-2 text-center p-2">
-        <div>
-
-          <input 
-            type="text" 
-            className="flex justify-between border px-4 py-2 rounded-lg focus:outline-none focus:border-blue-400"
-            placeholder='Todo'/>
+        <input 
+          type="text" 
+          className="flex justify-between border px-4 py-2 rounded-lg focus:outline-none focus:border-blue-400"
+          placeholder='Todo'/>
 
           <div className="flex justify-content flex-end">
             <select 
@@ -104,10 +111,31 @@ export default function Home() {
             <button type="button" className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border      border-transparent font-semibold text-blue-500 hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">
               +
             </button>
+          
           </div>
-            
-            
-        </div>
+      </div>
+
+      <div className="mx-auto my-3 w-9/12 bg-whiteborder rounded-lg border-gray-300 border-2 text-center p-2">
+        <input 
+          type="text" 
+          className="flex justify-between border px-4 py-2 rounded-lg focus:outline-none focus:border-blue-400"
+          placeholder='Todo'/>
+
+          <div className="flex justify-content flex-end">
+            <select 
+              id="hs-select-label" 
+              className="py-3 px-4 pr-9 block border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400">
+              <option selected>-Status-</option>
+                <option>Waiting</option>
+                <option>Doing</option>
+                <option>Done</option>
+            </select>
+          
+            <button type="button" className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border      border-transparent font-semibold text-purple-500 hover:bg-purple-100 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">
+              Save
+            </button>
+          
+          </div>
       </div>
       
     </div>
