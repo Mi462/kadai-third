e0610723b5d381f9aa19306aea1c4f5ccefda141
yumi_23817 <candyclover.12815@gmail.com>
2023-08-29 07:47:05 +0900

    async/awaitの質問

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -3,10 +3,11 @@
 import db from "@/lib/firebase/firebase";
 import { collection, query, where, doc, getDoc, setDoc, getDocs } from "firebase/firestore";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+//import { useEffect, useState } from "react";
 
-export default function Edit({ params }: { params: { id: string } }){
+export default async function Edit({ params }: { params: { id: string } }){
 
+  //渡ってきたid
   console.log(params.id)
 
   const OPTION_VALUES = ["-Status-", "Waiting", "Doing", "Done"];
@@ -18,68 +19,31 @@ export default function Edit({ params }: { params: { id: string } }){
     status: string; 
   }
 
-
-  // doc(db, "users").where("id", "==", "caa3820f-27fc-4ad2-af97-d0d5f373825a")
-  //   .get()
-  //   .then((querySnapshot: any) => {
-  //     querySnapshot.forEach( (doc: any) => {
-  //       console.log(doc.id, " => ", doc.data());
-  //     });
-  //   })
-  //   .catch( (error: any) => {
-  //     console.log(`データの取得に失敗しました (${error})`);
-  //   });
-
-  const [editTodos, setEditTodos] = useState<Todo>({
-    id: params.id,
-    text: "",
-    isEditing: false,
-    status: "waiting" 
-  });
-
-  const citiesRef = collection(db, "cities");
-
-   setDoc(doc(citiesRef, "SF"), {
-    name: "San Francisco", state: "CA", country: "USA",
-    capital: false, population: 860000,
-    regions: ["west_coast", "norcal"] });
-   setDoc(doc(citiesRef, "LA"), {
-    name: "Los Angeles", state: "CA", country: "USA",
-    capital: false, population: 3900000,
-    regions: ["west_coast", "socal"] });
-   setDoc(doc(citiesRef, "DC"), {
-    name: "Washington, D.C.", state: null, country: "USA",
-    capital: true, population: 680000,
-    regions: ["east_coast"] });
-   setDoc(doc(citiesRef, "TOK"), {
-    name: "Tokyo", state: null, country: "Japan",
-    capital: true, population: 9000000,
-    regions: ["kanto", "honshu"] });
-   setDoc(doc(citiesRef, "BJ"), {
-    name: "Beijing", state: null, country: "China",
-    capital: true, population: 21500000,
-    regions: ["jingjinji", "hebei"] });
-
-    const docRef = doc(db, "cities", "SF");
-    const docSnap = getDoc(docRef);
-
-    console.log("Document data:", docSnap.data());
-
   // const docRef = doc(db, "data", "caa3820f-27fc-4ad2-af97-d0d5f373825a");
-  // const docSnap: any = getDoc(docRef);
+  // const docSnap: any = await getDoc(docRef);
   // console.log(docSnap.data());
 
+  const docRef = doc(db, "data", params.id);
+  const docSnap: any = await getDoc(docRef);
+  //渡ってきたidを元にデータベースからデータを取り出してきた
+  console.log(docSnap.data());
 
   //Editボタン押下時に
   //下のEdit用のinputタグの中に編集内容を渡す(inputタグのvalueに編集内容を渡す)
   //対象のTodoリストを削除する
 
+  //console.log(id)
+  //渡されたidを利用して、データベースからデータを取ってくる
+    // const docRef = doc(db, "data", id);
+    // const docSnap: any = await getDoc(docRef);
+    //データベースから取ってきたデータ
+    // console.log(docSnap.data());
+
   const clickEdit = (id: string) => {
-    console.log(id)
     // データベースのデータを更新して編集
     // setDoc(doc(db, "data", id), {
     //   id: id,
-    //   text: editTodos.text,
+    //   text: defaultValue,
     //   isEditing: false,
     //   status: "waiting",
     // });
@@ -111,10 +75,10 @@ export default function Edit({ params }: { params: { id: string } }){
           {/* 入力欄 */}
           <input 
             type="text" 
-            //value={editTodos.text}
+            defaultValue={docSnap.data().text}
             className="flex justify-between w-full border px-4 py-2 rounded-lg focus:outline-none focus:border-blue-400"
             placeholder='Todo'
-            onChange={(e) => setEditTodos({...editTodos, text: e.target.value})}/>
+          />
           {/* 入力欄 */}
         
           {/* stateの選択肢・ボタン */}
@@ -137,7 +101,8 @@ export default function Edit({ params }: { params: { id: string } }){
             <button 
               type="button" 
               className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border      border-transparent font-semibold text-purple-500 hover:bg-purple-100 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
-              onClick={() => clickEdit(editTodos.id)}>
+              onClick={() => clickEdit(params.id)}
+            >
               Save
             </button>
             {/* Saveボタン  */}
